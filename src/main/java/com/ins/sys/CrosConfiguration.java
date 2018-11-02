8a2f7d63f0a65de49c56cd1cea27b79059c80f0e
高秀和 <15620952172@163.com>
2018-11-02 14:50:56 +0800

    1102

---
+++ src/main/java/com/ins/sys/CrosConfiguration.java
@@ -0,0 +1,28 @@
+package com.ins.sys;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CrosConfiguration {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        final UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+<<<<<<< HEAD
+        return new CorsFilt er(urlBasedCorsConfigurationSource);
+=======
+        return new CorsFilter(urlBasedCorsConfigurationSource);
+>>>>>>> 800df9e499a75354f348ab3a9bb120d65adbca08
+    }
+
+}
